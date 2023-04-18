23af42944446f76896b693f0273cdf53519fe663
johndoe <johndoe@example.com>
2023-04-18 16:06:36 +0200

    makdown poem to json

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+// UTILS
+
+const generateRandomId = () => {
+  const date = new Date();
+  const timestamp = date.getTime();
+  return "poem" + timestamp + Math.floor(Math.random() * 1000);
+}
+
+
+/**
+ * 
+ * @param {string} url 
+ * @returns 
+ */
+const getMarkdownFileNameFromUrl = (url) => {
+  if (url === null || url === undefined) { return generateRandomId(); }
+  const regex = /\/([^/]+)\.md/;
+  const match = url.match(regex);
+  return match ? match[1] : generateRandomId();
+}
+
+
+
+
+/////
+
+const poemStringHTMLtoJson = (content, urlMarkdownFile) => {
+  const extractParagraphFromString = (str) => {
+    const regex = /<p>.*?<\/p>/gs;
+    return str.match(regex);
+  }
+
+  const isTitle = (strTitle) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(strTitle, "text/html");
+    const strongTag = doc.querySelector("strong");
+    return strongTag !== null ? strongTag.textContent.trim() : '';
+  }
+  
+  const isStrophe = (strophes) => {
+    const parser = new DOMParser();
+    let stropheArray = [];    
+    strophes.forEach((strophe) => {
+      const _strophe = parser.parseFromString(strophe, "text/html").querySelector("p");
+      let verses = _strophe.textContent.split("\n");
+      stropheArray.push(verses);
+    });
+    return stropheArray;
+  }
+
+  const allParagraphs = extractParagraphFromString(content);
+  let poemJson = {
+    id: getMarkdownFileNameFromUrl(urlMarkdownFile),
+    title: isTitle(allParagraphs[0]),
+    strophes: isStrophe(allParagraphs.slice(1))
+  }
+  console.info(poemJson)
+
+}
+
+
+
+
 const initShowdown = (url, ok, ko) => {
   if (showdown === null) return;
   const converter = new showdown.Converter();
@@ -18,16 +81,16 @@ const initShowdown = (url, ok, ko) => {
 
 
 
+
+
+
+
 console.log("***********initShowdown************");
-const poema = "md/poem_with_spaces.md";
-initShowdown(poema, (content) => {
-  const scope = document.querySelector('#outputShowdown');
-  if (scope === null || content === null) return;
-  scope.innerHTML = content;
+const poemUrl = "md/verde_que_te_quiero_verde.md";
+initShowdown(poemUrl, (content) => {
+  console.log(content)
+  poemStringHTMLtoJson(content, poemUrl);
 });
-const poemaSinEspacios = "md/poem_without_spaces.md";
-initShowdown(poemaSinEspacios, (content) => {
-  const scope = document.querySelector('#outputShowdown2');
-  if (scope === null || content === null) return;
-  scope.innerHTML = content;
-});
+
+
+
